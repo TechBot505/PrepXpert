f18566d48cd821d438ebee6ecdb9a040b353bdc0
Rohit <rohitdhanotia2003@gmail.com>
2025-02-04 18:43:08 +0530

    Created Sections on Landing Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,10 @@
 import HeroSection from "@/components/hero";
+import CTA from "@/components/cta";
+import FAQS from "@/components/faqs";
+import Testimonials from "@/components/testimonials";
+import HowItWorks from "@/components/howItWorks";
+import Stats from "@/components/stats";
+import Features from "@/components/features";
 
 export default function Home() {
   return (
@@ -6,6 +12,18 @@ export default function Home() {
       <div className="grid-background"></div>
 
       <HeroSection />
+
+      <Features />
+
+      <Stats />
+
+      <HowItWorks />
+
+      <Testimonials />
+
+      <FAQS />
+
+      <CTA />
     </div>
   );
 }
