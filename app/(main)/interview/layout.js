413358f314817a7450f00c63da5d40e10c2588e2
Rohit <rohitdhanotia2003@gmail.com>
2025-03-25 09:37:54 +0530

    Assesments and Result Page

--- app/(main)/interview/layout.js
+++ app/(main)/interview/layout.js
@@ -1,12 +1,9 @@
 import React, { Suspense } from 'react';
 import { BarLoader } from 'react-spinners';
 
-const DashboardLayout = ({children}) => {
+const InterviewLayout = ({children}) => {
     return (
         <div className='px-5'>
-            <div className='flex items-center justify-between mb-5'>
-                <h1 className='text-6xl gradient-title font-bold'>Industry Insights</h1>
-            </div>
             <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color='gray' />}>
                 {children}
             </Suspense>
@@ -14,4 +11,4 @@ const DashboardLayout = ({children}) => {
     )
 }
 
-export default DashboardLayout;
+export default InterviewLayout;
